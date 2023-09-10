4ced61b48b655eda9453bd9366f364dc63f708e1
nataliamv <nataliacs_mv@hotmail.com>
2023-09-10 16:03:33 -0300

    preparo banco postgresql

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "3502e6628320556d15305267639bd81a"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
