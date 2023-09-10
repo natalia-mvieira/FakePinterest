2a8509aa0f45ad3c320b41c88525243f3f31da0d
nataliamv <nataliacs_mv@hotmail.com>
2023-09-10 16:57:11 -0300

    retirada link_banco

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,11 +5,7 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-if os.getenv("DEBUG") == 0: #DEBUG = variável de ambiente no render
-    link_banco = os.getenv("DATABASE_URL") #no ambiente online do render
-else:
-    link_banco = "sqlite:///comunidade.db" #banco de dados usado fora do render
-app.config["SQLALCHEMY_DATABASE_URI"] = link_banco
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "3502e6628320556d15305267639bd81a"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
